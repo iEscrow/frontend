7a94349d7ca35d9220f86e3a68e7ab457059ecd0
JosueMoora <josuedavidmora99@gmail.com>
2023-08-28 18:26:51 -0400

    isuue #12 [Fix] Login and sign-in

--- src/app/components/login/form.jsx
+++ src/app/components/login/form.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import logo from "../../../assets/logo.svg";
 import mail from "../../../assets/login/mail.svg";
@@ -10,11 +10,11 @@ import WithApps from "./withApps";
 import Button from "./button";
 
 export default function Form() {
-    const [password, setPassword] = useState(false)
-    const togglePassword = (e)=> {
-        e.preventDefault() 
-        setPassword(!password)
-    }
+  const [password, setPassword] = useState(false);
+  const togglePassword = (e) => {
+    e.preventDefault();
+    setPassword(!password);
+  };
   return (
     <form className="max-md:px-4">
       <div className="bg-blue w-full flex justify-center py-5 rounded-t-xl">
@@ -22,29 +22,33 @@ export default function Form() {
       </div>
       <div className=" shadow-[0_1px_2px_0_rgba(0,0,0,0.25)] bg-light-blue/50 rounded-b-xl flex flex-col items-center px-[60px]">
         <h2 className="my-6 md:text-2xl sm:text-lg font-semibold">Log In</h2>
-        <div className="flex w-full text-dark-gray">
-            <Image src={mail} className="absolute my-4 ml-4" alt="mail" />
-          <input
-            className="bg-white border-none rounded-xl py-3 mb-4 w-full pl-11 focus:outline-none"
-            type="email"
-            placeholder="Email"
-          />
+        <div className="flex flex-col gap-2">
+          <div className="flex w-full bg-white rounded-xl p-3 gap-1 items-center  text-dark-gray">
+            <Image src={mail} alt="mail" />
+            <input
+              className="bg-white border-none bg-transparent focus:outline-none"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex bg-white rounded-xl p-3 gap-1  text-dark-gray">
+            <Image src={lock} alt="lock" />
+            <input
+              className="bg-white border-none bg-transparent w-[80%] focus:outline-none"
+              type={password ? "text" : "password"}
+              placeholder="Password"
+            />
+            <button onClick={togglePassword}>
+              <Image src={view} alt="view" />
+            </button>
+          </div>
         </div>
-        <div className="flex w-full text-dark-gray">
-            <Image src={lock} className="absolute my-3 ml-4" alt="mail" />
-          <input
-            className="bg-white border-none rounded-xl py-3 mb-4 w-full pl-11 focus:outline-none"
-            type={password ? "text" : "password"}
-            placeholder="Password"
-          />
-          <button onClick={togglePassword} className="absolute my-4 md:ml-56 sm:ml-52">
-          <Image src={view} alt="view"/>
-          </button>
-        </div>
-        <Link href="login/accountRecovery" className="hover:underline">Forgot password?</Link>
-        <Button path="/" mode="login"/>
+        <Link href="login/accountRecovery" className="hover:underline">
+          Forgot password?
+        </Link>
+        <Button path="/" mode="login" />
         <div>
-          <div className="flex items-center justify-center md:text-sm sm:text-xs w-full mb-7">
+          <div className="flex items-center justify-center md:text-sm sm:text-xs w-full pb-7">
             <input
               type="checkbox"
               className="checkbox checkbox-xs border-white border mr-2"
@@ -52,7 +56,7 @@ export default function Form() {
             <span> Remember me</span>
           </div>
           <WithApps mode="Log In" />
-          <div className="flex gap-2 mb-4 sm:text-xs">
+          <div className="flex justify-center gap-2 pb-4 sm:text-xs">
             <h2>Don&#39;t have an account</h2>
             <Link href="/signin" className="underline">
               Create One
--- src/app/components/login/register.jsx
+++ src/app/components/login/register.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import logo from "../../../assets/logo.svg";
 import view from "../../../assets/login/view.svg";
@@ -7,61 +7,59 @@ import WithApps from "./withApps";
 import { useState } from "react";
 export default function Register() {
   const ph = [
-    {type:"text",
-    placeholder: "Name"
-    },
-    {type:"text",
-    placeholder: "Last name"
-    },
-    {type:"email",
-    placeholder: "Email"
-    }
-  ]
-  const [password, setPassword] = useState(false)
-    const togglePassword = (e)=> {
-        e.preventDefault() 
-        setPassword(!password)
-    }
+    { type: "text", placeholder: "Name" },
+    { type: "text", placeholder: "Last name" },
+    { type: "email", placeholder: "Email" },
+  ];
+  const [password, setPassword] = useState(false);
+  const togglePassword = (e) => {
+    e.preventDefault();
+    setPassword(!password);
+  };
   return (
     <form>
       <div className="bg-blue w-full flex justify-center py-5 rounded-t-xl">
         <Image width={144} height={62.4} src={logo} alt="logo" />
       </div>
-      <div className="bg-[#37d1f44f] text-white rounded-b-xl flex flex-col items-center px-[60px]">
-        <h2 className="my-6 md:text-2xl sm:text-lg font-semibold">Create account</h2>
-        {ph.map(p => {
+      <div className="bg-[#37d1f44f] text-white rounded-b-xl flex flex-col gap-4 p-4  items-center px-[60px]">
+        <h2 className="md:text-2xl sm:text-lg font-semibold">Create account</h2>
+        {ph.map((p) => {
           return (
             <input
-            key={p.placeholder}
-          className="bg-white border-none rounded-full sm:text-sm text-dark-gray py-3 mb-4 w-full pl-4 focus:outline-none"
-          type={p.type}
-          placeholder={p.placeholder}
-        />
-          )
+              key={p.placeholder}
+              className="bg-white border-none rounded-full sm:text-sm text-dark-gray py-3 w-full pl-4 focus:outline-none"
+              type={p.type}
+              placeholder={p.placeholder}
+            />
+          );
         })}
-        <div className="flex w-full text-dark-gray">
+        <div className="flex w-full items-center sm:text-sm text-dark-gray bg-white rounded-full gap-1 p-3 ">
           <input
-            className="bg-white border-none rounded-full py-3 mb-3 w-full pl-4 focus:outline-none"
+            className="bg-transparent border-none w-[80%] focus:outline-none"
             type={password ? "text" : "password"}
             placeholder="Password"
           />
-          <button onClick={togglePassword} className="absolute my-4 md:ml-56 sm:ml-52">
-          <Image src={view} alt="view"/>
+          <button
+            onClick={togglePassword}
+            className="absolute my-4 md:ml-56 sm:ml-52"
+          >
+            <Image src={view} alt="view" />
           </button>
         </div>
-        <div className="flex w-full text-dark-gray">
+        <div className="flex w-full items-center sm:text-sm text-dark-gray bg-white rounded-full gap-1 p-3 ">
           <input
-            className="bg-white border-none rounded-full py-3 mb-3 w-full pl-4 focus:outline-none"
+            className="bg-transparent border-none w-[80%] focus:outline-none"
             type={password ? "text" : "password"}
             placeholder="Confirm Password"
           />
-          <button onClick={togglePassword} className="absolute my-4 md:ml-56 sm:ml-52">
-          <Image src={view} alt="view"/>
+          <button
+            onClick={togglePassword}
+            className="absolute my-4 md:ml-56 sm:ml-52"
+          >
+            <Image src={view} alt="view" />
           </button>
         </div>
-        <div className="mb-3">
-          <Button path="signin/confirmEmail" mode="register" />
-        </div>
+        <Button path="signin/confirmEmail" mode="register" />
         <WithApps mode="Sign In" />
       </div>
     </form>
