aefe99c817ace8b9980c64cccbe6522ac36d7028
Servezynski <servezynskimarcos1@gmail.com>
2023-09-13 23:03:16 -0300

    agrego menu mobile y menu wallet

--- src/app/components/Footer/footerMobile.jsx
+++ src/app/components/Footer/footerMobile.jsx
@@ -7,15 +7,15 @@ import { useState } from "react";
 import Link from "next/link";
 
 export default function FooterMobile() {
-  const [more, setMore] = useState({div1: true, div2:true, div3:true});
+  const [more, setMore] = useState({ div1: true, div2: true, div3: true });
   const handleClick = (index) => {
-    setMore(p => ({
-        ...p,
-        [index]:!p[index]
-    }))
+    setMore((p) => ({
+      ...p,
+      [index]: !p[index],
+    }));
   };
   return (
-    <footer className=" md:hidden p-5 bg-black">
+    <footer className=" md:hidden p-5 bg-black ">
       <div className="collapse" onClick={() => handleClick("div1")}>
         <input type="checkbox" />
         <div className="flex  justify-between collapse-title text-sm font-medium uppercase text-blue-2">
@@ -55,22 +55,19 @@ export default function FooterMobile() {
         </div>
       </div>
       <hr className="border-dark-gray" />
-      <div className="flex  flex-col text-sm font-medium uppercase py-3 pl-4 text-blue-2">
-        Comunidad
-        <div className="flex flex-col items-center">
-          <div className="grid grid-cols-3 gap-8">
-            {icons.map((i) => {
-              return (
-                <Link
-                  key={i.path}
-                  href={i.path}
-                  className="bg-blue-2 rounded-full p-1"
-                >
-                  <Image src={i.img} alt=" " />
-                </Link>
-              );
-            })}
-          </div>
+      <div className="flex flex-col items-center w-full p-4 mb-10">
+        <div className="flex w-full justify-around">
+          {icons.map((i) => {
+            return (
+              <Link
+                key={i.path}
+                href={i.path}
+                className="bg-blue-2 rounded-full p-1"
+              >
+                <Image src={i.img} alt=" " />
+              </Link>
+            );
+          })}
         </div>
       </div>
       <hr className="border-dark-gray" />
