6cd0419e7414f2b529a0faf0ed8c7b9cbc2df35d
Servezynski <servezynski@gmail.com>
2024-01-08 18:53:41 -0300

    Aplico cambios minimos en estilos

--- src/app/depositos/layout.js
+++ src/app/depositos/layout.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const layout = ({ children }) => {
   return (
-    <div className="min-h-screen w-full px-20 pb-20 flex flex-col sm:gap-6 md:gap-10">
-      <div>
+    <div className="min-h-screen w-full px-20 pb-20 flex flex-col sm:gap-6 md:gap-20">
+      <div className="w-[15%]">
         <a href="/">
           <Image src={logo} alt="logo" />
         </a>
